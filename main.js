afbed1dbffd2238a22afb8fcb85de7f5dcca77b9
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-12-12 07:54:00 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -3,7 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal")
+modal.classList.add("hidden")
 
+
+const heart = document.querySelectorAll(".like-glyph")
+// hearts.forEach(heart => heart.addEventListener("click", click))
+for (let i = 0; i<heart.length; i++) {
+  heart[i].addEventListener("click", (e) => {
+    let heartParent = e.target
+    click(event)
+    console.log(event)
+  })
+  
+}
+
+const click = event => {
+  const heart = event.target
+  heart.innerText = EMPTY_HEART
+  mimicServerCall()
+      .then(res => {
+        if(heart.innerText === EMPTY_HEART){
+          heart.innerText = FULL_HEART
+          heart.classList.add("activated-heart")
+        }
+        else{
+          heart.innerText = EMPTY_HEART
+          heart.classList.remove("activated-heart")
+        }
+
+      })
+    
+    
+      .catch(err => {
+        modal.classList.remove("hidden")
+        modal.querySelector("#modal-message").textContent= err
+
+        setTimeout(() => {
+          modal.classList.add("hidden")
+      }, 3000)
+
+})
+}
 
 
 
